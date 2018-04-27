71c478174e9aa8d246e384607d2ad4825391e88d
Koteswarao <kotesh@kvana.com>
2018-04-27 14:16:03 +0530

    validation completed

---
+++ app/src/main/java/com/koti/apple/loginsqlite/MainActivity.java
@@ -0,0 +1,38 @@
+package com.koti.apple.loginsqlite;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button save_button;
+    EditText name_et,password_et;
+    private SQLiteDatabase sqLiteDatabase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        save_button= (Button)findViewById(R.id.save_button);
+        name_et = (EditText)findViewById(R.id.name_et);
+        password_et = (EditText)findViewById(R.id.password_et);
+        SaveLoginDatabase saveLoginDatabase = new SaveLoginDatabase(this);
+        sqLiteDatabase = saveLoginDatabase.getWritableDatabase();
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String validCredentials = new SaveLoginDatabase(getApplicationContext()).getUserInfo(sqLiteDatabase, name_et.getText().toString(), name_et.getText().toString());
+                if (validCredentials.equalsIgnoreCase("nullnull") && validCredentials.length() > 0){
+                    new SaveLoginDatabase(getApplicationContext()).addUser(sqLiteDatabase, name_et.getText().toString(), name_et.getText().toString());
+                    Toast.makeText(MainActivity.this, "user data saved successfully ", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "user login exists ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/koti/apple/loginsqlite/SaveLoginDatabase.java
@@ -0,0 +1,150 @@
+package com.koti.apple.loginsqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaveLoginDatabase extends SQLiteOpenHelper {
+    //database helper
+    public static final String DATABASE_NAME_ = "users.db";
+    public static final int DATABASE_VERSION = 1;
+
+    //User table
+    public static final String TABLE_USERS = "users";
+
+    //users table rows
+    public static final String USERS_COLUMN_ID = "ID";
+    public static final String USERS_COLUMN_NAME = "name";
+    public static final String USERS_COLUMN_PASSWORD = "password";
+
+    //quarries
+    //create
+    public static final String CREATE_TABLES_USERS = "create table " + TABLE_USERS +
+            "(" +
+            USERS_COLUMN_ID + " integer primary key," +
+            USERS_COLUMN_NAME + " text," +
+            USERS_COLUMN_PASSWORD + " text)";
+
+    //drop
+    public static final String DROP_TABLES_USERS = "DROP TABLE IF EXISTS " + TABLE_USERS;
+
+    //select rows
+    public static final String SELECT_ALL_USERS = "SELECT * FROM " + TABLE_USERS;
+
+    //array of columns
+    private static String[] USER_COLUMNS = {USERS_COLUMN_ID, USERS_COLUMN_NAME, USERS_COLUMN_PASSWORD};
+
+
+    public SaveLoginDatabase(Context context) {
+        super(context, DATABASE_NAME_, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLES_USERS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //delete old tables
+        db.execSQL(DROP_TABLES_USERS);
+        //creating tables again
+        onCreate(db);
+    }
+
+    /**
+     * it inserts user to table
+     *
+     * @param sqLiteDatabase writable db
+     * @param name
+     * @param password
+     */
+    public void addUser(SQLiteDatabase sqLiteDatabase, String name, String password) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(USERS_COLUMN_NAME, name);
+        contentValues.put(USERS_COLUMN_PASSWORD, password);
+        //insert
+        sqLiteDatabase.insert(TABLE_USERS, null, contentValues);
+        Log.e("info >> ", "addUser : inserted ");
+    }
+
+    public String getUserInfo(SQLiteDatabase sqLiteDatabase, String name, String password) {
+        try {
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT name, password FROM users WHERE name = ? AND password = ?", new String[]{name, password});
+
+            String nameVal = null, passwordVal = null;
+            if (cursor != null) {
+                int i = 0;
+                String nameCol[] = new String[cursor.getCount()];
+                String passwordCol[] = new String[cursor.getCount()];
+                if (cursor.getCount() > 0) {
+                    cursor.moveToFirst();
+                    do {
+                        nameCol[i] = cursor.getString(cursor.getColumnIndex("name"));
+                        passwordCol[i] = cursor.getString(cursor.getColumnIndex("password"));
+                        Log.e("info >> ", "name  :  " + nameCol[i]);
+                        Log.e("info >> ", "password  :  " + passwordCol[i]);
+                        nameVal = nameCol[i];
+                        passwordVal = passwordCol[i];
+                        i++;
+                    } while (cursor.moveToNext());
+                    cursor.close();
+                }
+            }
+            return nameVal + passwordVal;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * returns all users
+     */
+  /*  public List<UserModel> getAllUsers(SQLiteDatabase sqLiteDatabase) {
+        List<UserModel> userModels = new ArrayList<>();
+        //get cur
+        Cursor cursor = sqLiteDatabase.query(TABLE_USERS, USER_COLUMNS, null, null, null, null, USERS_COLUMN_ID + " ASC");
+        while (cursor.moveToNext()) {
+            UserModel userModel = new UserModel();
+            userModel.setId(cursor.getInt(cursor.getColumnIndex(USERS_COLUMN_ID)));
+            userModel.setName(cursor.getString(cursor.getColumnIndex(USERS_COLUMN_NAME)));
+            userModel.setEmail(cursor.getString(cursor.getColumnIndex(USERS_COLUMN_EMAIL)));
+            userModel.setMobile(cursor.getString(cursor.getColumnIndex(USERS_COLUMN_MOBILE)));
+            userModels.add(userModel);
+
+        }
+
+        return userModels;
+    }*/
+    public void updateUser(SQLiteDatabase sqLiteDatabase, int id, String name, String email, String mobile) {
+        /*ContentValues contentValues = new ContentValues();
+        contentValues.put(USERS_COLUMN_NAME, name);
+        contentValues.put(USERS_COLUMN_MOBILE, mobile);
+        contentValues.put(USERS_COLUMN_EMAIL, email);
+        sqLiteDatabase.update(
+                TABLE_USERS,
+                contentValues,
+                USERS_COLUMN_ID + " = ?",
+                new String[]{String.valueOf(id)});*/
+    }
+
+    public void deleteUser(SQLiteDatabase sqLiteDatabase, int id) {
+        sqLiteDatabase.delete(
+                TABLE_USERS,
+                USERS_COLUMN_ID + " = ?",
+                new String[]{String.valueOf(id)});
+
+    }
+
+    public void deleteAllUsrs(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.delete(TABLE_USERS, null, null);
+    }
+}
